15cd04e590dc3cff243703fbef6ac830432f5694
KristijanArmeni <kristijan.armeni@gmail.com>
2025-06-26 22:18:27 -0400

    use STORIES_2 for chunk_boostrap implementation, code style

--- src/encoders/run_all.py
+++ src/encoders/run_all.py
@@ -94,11 +94,13 @@ def run_all(
     interpolation : {"lanczos", "average"}, default="lanczos"
         Whether to use lanczos interpolation or just average the words within a TR.
         Only applies if `predictor=eng1000`.
-    ridge_implementation: {"ridgeCV", "ridge_chunkbootstrap", "ridge_huth"}, default="ridge_huth"
+    ridge_implementation: {"ridgeCV", "ridge_chunkbootstrap", "ridge_huth"},
+        default="ridge_huth"
         Which ridge regression implementation to use.
         `ridgeCV` will use scikit-learn's RidgeCV.
-        `ridge_chunkbootstrap` will use the chunked bootstrap method from Lebel et al. with scikit-learn's Ridge.
-        `ridge_huth` will use the ridge regression implementation from Lebel et al.
+        `ridge_chunkbootstrap` will use the chunked bootstrap method from Lebel et al.
+        with scikit-learn's Ridge. `ridge_huth` will use the ridge regression
+        implementation from Lebel et al.
     do_shuffle: book, default=False
         Whether or not to run model fits with predictors shuffled (as a control).
         A separate subfolder ('shuffled') will be created in the run folder with
@@ -178,9 +180,7 @@ def run_all(
     # pick the right pool of stories, depending on ridge implementation
     if ridge_implementation == "ridge_huth":
         stories = load_config()["STORIES"].copy()
-    elif ridge_implementation == "ridge_chunkbootstrap":
-        stories = load_config()["STORIES"].copy()
-    elif ridge_implementation == "ridgeCV":
+    elif ridge_implementation in ["ridgeCV", "ridge_chunkbootstrap"]:
         stories = load_config()["STORIES_2"].copy()
 
     if not isinstance(stories, list):
